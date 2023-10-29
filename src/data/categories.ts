832b69f21a66d5ac741bab836b2d691c4789be84
Hygor Gomes <hygorgabrielg@gmail.com>
2023-10-29 18:46:32 -0300

    Branch iteracao2

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from '../types/Category';
+
+export const categories: Category = {
+    food: { title: 'Outros', color: 'red', expense: true },
+    rent: { title: 'Despesas', color: 'brown', expense: true },
+    salary: { title: 'Recebimentos', color: 'green', expense: false }
+}
