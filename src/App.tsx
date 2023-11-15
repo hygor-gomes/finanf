50e536f7ef2055ef0cd423c118bbe21d5936123d
Hygor Gomes <hygorgabrielg@gmail.com>
2023-11-15 04:05:57 -0300

    Fixing gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/finanf" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/main" element={<RequireAuth>
           <>
